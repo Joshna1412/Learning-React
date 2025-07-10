170566c7d609eb4d3c9a22827dfbffc350501e68
Joshna1412 <djoshna167@gmail.com>
2025-07-10 18:18:42 +0530

    added hamburger-menu

---
+++ src/Home/index.jsx
@@ -0,0 +1,11 @@
+import './index.css'
+
+const Home = () => {
+    return (
+        <div className='home-container'>
+            <img className='home-image' src="https://assets.ccbp.in/frontend/react-js/home-lg-img.png " alt="home-img" />
+        </div>
+    )
+}
+
+export default Home
